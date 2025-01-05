7e910a9f800a15c4f351aaf26f6895217c021586
kanav789 <kanavrana91@gmail.com>
2025-01-05 13:05:57 +0530

    creating UserModel in backend

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minLength: [3, "First name must be at least 3 characters long"],
+    },
+    lastname: {
+      type: String,
+
+      minLength: [3, "Last name must be at least 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
+});
+userSchema.methods.generateJWT = function () {
+  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET);
+  return token;
+};
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+const UserModel = mongoose.model("User", userSchema);
+module.exports = UserModel;
