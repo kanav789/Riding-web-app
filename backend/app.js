f13b2fe7ba20ac168ea54accf491583f498a7d71
kanav789 <kanavrana91@gmail.com>
2025-01-05 16:00:02 +0530

    setting up the cookies

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connect = require("./Db/db.js");
-
+const cookieParser = require("cookie-parser");
 dotenv.config();
 connect();
 const app = express();
@@ -9,6 +9,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 // Routers
 
 const userrouter = require("./routes/userRoutes.js");
