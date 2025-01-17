fe72b9c338808ab53c1b1139c2026c203cb2bd4a
kanav789 <kanavrana91@gmail.com>
2025-01-17 22:17:54 +0530

    creating a routes in frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import UserLogin from "./Pages/UserLogin";
+import UserSignup from "./Pages/UserSignup";
+import CaptainLogin from "./Pages/CaptainLogin";
+import CaptainSignup from "./Pages/CaptainSignup";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/user-signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
