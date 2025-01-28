82d892460ad76ecd9dd52a88e8aa83bdffe7b381
kanav789 <kanavrana91@gmail.com>
2025-01-28 19:52:26 +0530

    creating the loaction search panel

---
+++ Frontend/src/Components/LoactionSearchPanel.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function LoactionSearchPanel() {
+  return (
+    <div>
+      <div className="flex items-center justify-start gap-4 p-5">
+        <h2 className="bg-[#eee] h-10 w-10 rounded-full flex justify-center items-center">
+          <i className="ri-map-pin-line"></i>
+        </h2>
+        <h4 className="flex flex-wrap text-base font-medium font-sans">
+          Ward No.1 , Kabir Calony near Kali Ki Dukaan Allah, Jammu.
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default LoactionSearchPanel;
