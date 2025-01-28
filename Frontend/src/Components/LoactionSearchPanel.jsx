c733105ba3c0029c17de5cda4421629c20232455
kanav789 <kanavrana91@gmail.com>
2025-01-29 00:19:42 +0530

    location search panel

--- Frontend/src/Components/LoactionSearchPanel.jsx
+++ Frontend/src/Components/LoactionSearchPanel.jsx
@@ -3,8 +3,26 @@ import React from "react";
 function LoactionSearchPanel() {
   return (
     <div>
-      <div className="flex items-center justify-start gap-4 p-5">
-        <h2 className="bg-[#eee] h-10 w-10 rounded-full flex justify-center items-center">
+      <div className="flex items-center justify-start gap-4 p-5 m-4 border-2 border-gray-100 active:border-black rounded-lg">
+        <h2 className="bg-[#eee] h-8 w-12 rounded-full flex justify-center items-center">
+          <i className="ri-map-pin-line"></i>
+        </h2>
+        <h4 className="flex flex-wrap text-base font-medium font-sans">
+          Ward No.1 , Kabir Calony near Kali Ki Dukaan Allah, Jammu.
+        </h4>
+      </div>
+
+      <div className="flex items-center justify-start gap-4 p-5 m-4 border-2 border-gray-100 active:border-black rounded-lg">
+        <h2 className="bg-[#eee] h-8 w-12 rounded-full flex justify-center items-center">
+          <i className="ri-map-pin-line"></i>
+        </h2>
+        <h4 className="flex flex-wrap text-base font-medium font-sans">
+          Ward No.1 , Kabir Calony near Kali Ki Dukaan Allah, Jammu.
+        </h4>
+      </div>
+
+      <div className="flex items-center justify-start gap-4 p-5 m-4 border-2 border-gray-100 active:border-black rounded-lg">
+        <h2 className="bg-[#eee] h-8 w-12 rounded-full flex justify-center items-center">
           <i className="ri-map-pin-line"></i>
         </h2>
         <h4 className="flex flex-wrap text-base font-medium font-sans">
