f2723da8f8ac692ae9fdcd64afcd534b3439a7f7
kanav789 <kanavrana91@gmail.com>
2025-02-26 23:02:11 +0530

    adding the socket provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import UserContext from "./context/UserContext.jsx";
+
+import  SocketProvider  from "./context/SocketContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserContext>
+    <SocketProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </UserContext>
+    </SocketProvider>
   </StrictMode>
 );
